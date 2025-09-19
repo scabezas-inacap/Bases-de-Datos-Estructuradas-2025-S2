82f8bf469dae778421fcef6aac11bf0c2f38501e
Sebastián Cabezas Ríos <68340396+scabezas-academico@users.noreply.github.com>
2025-09-19 16:51:57 -0300

    solo HR

--- Esquema HR Oracle/HR_01_CREATE_USER_HR.sql
+++ Esquema HR Oracle/HR_01_CREATE_USER_HR.sql
@@ -1,3 +1,12 @@
-ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
-create user hr identified by hr;
-grant DBA to hr;
+CREATE USER hr IDENTIFIED BY hr
+DEFAULT TABLESPACE users
+TEMPORARY TABLESPACE temp
+ACCOUNT UNLOCK;
+
+GRANT CREATE SESSION TO hr;        -- puede conectarse
+GRANT CREATE TABLE TO hr;          -- puede crear tablas
+GRANT CREATE VIEW TO hr;           -- puede crear vistas
+GRANT CREATE SEQUENCE TO hr;       -- puede crear secuencias
+GRANT CREATE PROCEDURE TO hr;      -- puede crear procedimientos
+GRANT CREATE TRIGGER TO hr;        -- puede crear triggers
+GRANT CREATE TYPE TO hr;           -- puede crear tipos de datos
